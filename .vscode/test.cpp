66857cfe154b6aac74beb6fba23b74cec100581d
lixiaofei <123456>
2022-09-02 22:36:56 +0800

    init

---
+++ .vscode/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"名称:";
+    cin>>name;
+    cout<<"is:"<<name<<endl;
+    cout<<"hello world";
+    return 0;
+}
